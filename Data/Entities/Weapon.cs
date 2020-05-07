8f0a2aa7e85fc6eef5b5f0d4c2689224a80198ef
SMackeyCoding <s.Mack.coding@gmail.com>
2020-05-07 19:19:18 -0400

    Added Weapon Data Class

---
+++ Data/Entities/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Weapon
+    {
+        [Key]
+        public int WeaponId { get; set; }
+        [Required]
+        public string WeaponName { get; set; }
+        [Required]
+        public string WeaponType { get; set; }
+        [Required]
+        public string WeaponRange { get; set; }
+        [Required]
+        public string WeaponColor { get; set; }
+        public string BladeOrEnergyColor { get; set; }
+        public string WeaponDamage { get; set; }
+        public int Price { get; set; }
+    }
+}
