7aa01b15d0edd5d4f33dee69e3dcb741e9257585
SMackeyCoding <s.Mack.coding@gmail.com>
2020-05-07 18:36:49 -0400

    Added Character Models

---
+++ Services/CharacterService.cs
@@ -0,0 +1,34 @@
+﻿using Data;
+using Data.Entities;
+using Models.CharacterModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CharacterService
+    {
+        public bool CreateCharacter(CharacterCreateModel characterToCreate)
+        {
+            var entity = new Character()
+            {
+                FirstName = characterToCreate.FirstName,
+                LastName = characterToCreate.LastName,
+                Species = characterToCreate.Species,
+                Price = characterToCreate.Price,
+                Affiliation = characterToCreate.Affiliation,
+                DefaultWeaponId = characterToCreate.DefaultWeaponId,
+                DefaultShipId = characterToCreate.DefaultShipId
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Characters.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
