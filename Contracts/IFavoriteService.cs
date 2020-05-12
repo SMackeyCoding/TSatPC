9f8abec178c6ee1ed5b4b2ac6b26ae492825074a
Loretta Tappan <lorettatappan@gmail.com>
2020-05-11 20:34:36 -0400

    Added interface

---
+++ Contracts/IFavoriteService.cs
@@ -0,0 +1,18 @@
+﻿using Models.FavoriteModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IFavoriteService
+    {
+        void CreateFavorite(FavoritesCreateModel favoriteToCreate);
+        IEnumerable<FavoritesListItem> GetAllFavorites();
+        FavoritesDetailModel GetFavoritesDetailById(int favoriteId);
+        void UpdateFavoritesById(int favoriteId, FavoritesUpdateModel favoriteToUpdate);
+        void DeleteFavoritesById(int favoriteId);
+    }
+}
