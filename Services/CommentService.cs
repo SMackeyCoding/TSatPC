7e08bee2803206a811c08758ff5d4c11646aced4
SMackeyCoding <s.Mack.coding@gmail.com>
2020-05-14 17:31:13 -0400

    commented comments

--- Services/CommentService.cs
+++ Services/CommentService.cs
@@ -10,36 +10,36 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-    public class CommentService : ICommentService
-    {
-        private readonly ApplicationDbContext _ctx = new ApplicationDbContext();
-        public void CreateComment(CommentCreateModel commentToCreate)
-        {
-            var entity = new Comment()
-            {
-                CharacterId = commentToCreate.CharacterId,
-                Character = 
-            }
-        }
+    //public class CommentService : ICommentService
+    //{
+    //    private readonly ApplicationDbContext _ctx = new ApplicationDbContext();
+    //    public void CreateComment(CommentCreateModel commentToCreate)
+    //    {
+    //        var entity = new Comment()
+    //        {
+    //            CharacterId = commentToCreate.CharacterId,
+    //            Character = 
+    //        }
+    //    }
 
-        public void DeleteCommentByCommentId(int commentId)
-        {
-            throw new NotImplementedException();
-        }
+    //    public void DeleteCommentByCommentId(int commentId)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public IEnumerable<CommentListModel> GetAllCommentsByCharacterId()
-        {
-            throw new NotImplementedException();
-        }
+    //    public IEnumerable<CommentListModel> GetAllCommentsByCharacterId()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public CommentDetailModel GetDetailedCommentsByCharacterId()
-        {
-            throw new NotImplementedException();
-        }
+    //    public CommentDetailModel GetDetailedCommentsByCharacterId()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public void UpdateCommentByCommentId(int commentId, CommentUpdateModel commentToUpdate)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //    public void UpdateCommentByCommentId(int commentId, CommentUpdateModel commentToUpdate)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
